5f5d580a802c231c853bf374627523decc9569eb
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-04-08 17:01:55 +0200

    ajout du carousel présentation

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let titreHome = document.getElementById('titreHome')
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY
+    if (value * 2 >= 400) {
+        titreHome.style.display = 'none';
+
+    } else {
+        titreHome.style.top = value * 2 + 'px';
+        titreHome.style.display = 'block';
+    }
+    console.log(value * 2)
+
+})
+
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+    // If we need pagination
+    pagination: {
+    el: ".swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar"
+    }
+});
