083da7c260da40ba6a1303a169d0cda170295909
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-04-10 14:33:57 +0200

    css

--- js/index.js
+++ js/index.js
@@ -19,10 +19,10 @@ const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
 
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    //   },
 
     // If we need pagination
     pagination: {
